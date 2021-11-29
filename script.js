60b653669a377454ca1a92ae13bf601953e225f2
Darion Tahmasebi <darion2124@gmail.com>
2021-11-28 16:12:06 -0800

    made changes

--- script.js
+++ script.js
@@ -1,68 +1,7 @@
-var box = document.querySelector('.box');
-targetElm = document.querySelector('.boxChild');
+// write a JavaScript program to check two numbers and return true if one of the number is 100 or if the sum of the two numbers is 100 
 
+const isEqualTo100 = (a,b) => a === 100 || b
+=== 100;
 
-
-var link1 = document.getElementById("link1");
-
-
-
-document.querySelector('button').addEventListener('click', function() {
-    scrollToElm(box, targetElm, 600);
-});
-
-var link1 = document.getElementById("link1");
-
-link1.scrollIntoView({
-    behavior: "smooth",
-    block:"start",
-    inline:"nearest"
-});
-
-
-
-function scrollToElm(container, elm, duration) {
-    var pos = getRelativePos(elm);
-    scrollTo(container, pos.top, 2);
-}
-
-function getRelativePos(elm) {
-    var pPos = elm.parentNode.getBoundingClientReact(),
-    cPos = elm.getBoundingClientReact(),
-    pos = {};
-
-    pos.top = cPos.top - pPos.top + elm.parentNode.scrollTop,
-    pos.right = cPos.right - pPos.right,
-    pos.bottom = cPos.bottom - pPos.bottom,
-    pos.left = cPos.left - pPos.left;
-
-    return pos;
-}
-
-function scrollTo(element, to, duration, onDone) {
-    var start = element.scrollTop,
-    change = to - start,
-    startTime = performance.now(),
-    val, now, elapsed, t;
-}
-
-
-function animationScroll() {
-    now = performance.now();
-    elapsed = (now - startTime) / 1000;
-    t = (elapsed/duration);
-
-    element.scrollTop = start + change * easeInOutQuad(t);
-
-    if ( t < 1 )
-    window.requestAnimationFrame(aniamtionScroll);
-    else 
-    onDone && onDone();
-};
-
-animateScroll();
-
-
-function easeInOutQuad(t) {
-    return t < .5 ? 2*t*t : -1+(4-2*t)*t 
-};
+console.log(isEqualTo100(100,0));
+console.log(isEqualTo100(0,100));
